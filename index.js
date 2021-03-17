f815ee728ba28ab79fbac440ea2bbf28f82283a2
kingdayx <54965488+kingdayx@users.noreply.github.com>
2021-03-17 16:49:21 -0400

    edited to remove metadata
    edited to only pass in the name the image url and the rarity type

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const fs = require('fs')
+const prompt = require('prompt-sync')()
 
 async function scrapeProduct() {
   let nftsData = []
   console.log('\nGetting files from gallery folder...')
   let files = fs.readdirSync(`${__dirname}/gallery`)
 
-  console.log('\nPreparing metadata directory...')
-  await fs.rmdirSync(`${__dirname}/metadata`, { recursive: true }, function (
-    err,
-  ) {
+  console.log('\nPreparing metadata directory...\n')
+  await fs.rmSync(`${__dirname}/metadata`, { recursive: true }, function (err) {
     if (err) throw err
     console.log('File is created successfully.')
   })
@@ -21,15 +20,17 @@ async function scrapeProduct() {
 
   console.log('\nCreating metadata...')
   for (let i = 0; i < files.length; i++) {
+    const name = prompt('What is the name of the picture? ')
+    const imageUrl = prompt('What is the name of the image url? ')
+    const rarity = prompt('What is the name of the what is the rarity? ')
     var img = fs.readFileSync(`${__dirname}/gallery/${files[i]}`, {
       encoding: 'base64',
     })
     let metadata = JSON.stringify(
       {
-        name: `${/[^.]*/.exec(files[i])[0]}`,
-        description: 'metadata for mistcop',
-        image: `${files[i]}`,
-        data: img,
+        name: `${name}`,
+        imageUrl: `${imageUrl}`,
+        attributes: [{ trait_type: 'Rarity', value: `${rarity}` }],
       },
       null,
       '\t',
