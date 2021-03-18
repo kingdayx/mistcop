d70cd32fb8fa0c3353aca76402eeee98abd79fd4
kingdayx <54965488+kingdayx@users.noreply.github.com>
2021-03-18 00:19:28 -0400

    added uploading to ipfs support
    gets link from ipfs

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const fs = require('fs')
+const ipfs = IpfsHttpClient('http://127.0.0.1:5001')
 const prompt = require('prompt-sync')()
 
 async function scrapeProduct() {
-  let nftsData = []
+  let files = fs.readdirSync(`${__dirname}/gallery`)
+  console.log('\nUploading images on IPFS...')
+  let upload = await ipfs.add(
+    globSource(`${__dirname}/gallery`, { recursive: true }),
+  )
   console.log('\nGetting files from gallery folder...')
   let files = fs.readdirSync(`${__dirname}/gallery`)
 
@@ -21,7 +26,6 @@ async function scrapeProduct() {
   console.log('\nCreating metadata...')
   for (let i = 0; i < files.length; i++) {
     const name = prompt('What is the name of the picture? ')
-    const imageUrl = prompt('What is the name of the image url? ')
     const rarity = prompt('What is the name of the what is the rarity? ')
     var img = fs.readFileSync(`${__dirname}/gallery/${files[i]}`, {
       encoding: 'base64',
@@ -29,7 +33,7 @@ async function scrapeProduct() {
     let metadata = JSON.stringify(
       {
         name: `${name}`,
-        imageUrl: `${imageUrl}`,
+        imageUrl: `https://ipfs.io/ipfs/${upload.cid.toString()}/${files[i]}`,
         attributes: [{ trait_type: 'Rarity', value: `${rarity}` }],
       },
       null,
